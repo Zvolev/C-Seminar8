ddb2f404e9a5997b2e848230a992103524fc3e09
Evgenii <zvolev@mail.ru>
2022-11-13 20:41:48 +0300

    add DZ2? DZ3

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 2: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+*/
+
+int Prompt(string message) // запрос и ввод значений
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] FillRandomMatrix(int line, int column, int argMin, int argMax) // наполняем матрицу случайными значениями по заданным параметрам и выводим на экран
+{
+    int[,] matrix = new int[line, column];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(argMin, argMax);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SumElementRowMatrix(int[,] matr, int row)
+{
+    int sumElementRow = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        sumElementRow = sumElementRow + matr[row, j];
+    }
+    return sumElementRow;
+}
+
+(int, int) MinSumElementRowMatrix(int[,] matr)
+{
+    int numRow = 0;
+    int minSum = SumElementRowMatrix(matr, 0);
+    for (int i = 1; i < matr.GetLength(0); i++)
+    {
+        int sumElementRow = SumElementRowMatrix(matr, i);
+        if (minSum > sumElementRow)
+        {
+            minSum = sumElementRow;
+            numRow = i;
+        }
+    }
+    return (minSum, numRow);
+}
+
+
+int line = Prompt("Укажите количество строк матрицы >- ");
+int column = Prompt("Укажите количество столбцов матрицы >- ");
+int min = Prompt("Укажите диапазон значений матрицы: от >- ");
+int max = Prompt("до (включительно) >- ");
+int[,] matrix = FillRandomMatrix(line, column, min, max + 1);
+PrintMatrix(matrix);
+(int minSumRow, int numRow) = MinSumElementRowMatrix(matrix);
+System.Console.WriteLine($"{numRow+1} cтрока с наименьшей суммой элементов => {minSumRow}");
---
+++ DZ3/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Задача 3: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+
+Две матрицы можно умножить, если число строк второй матрицы равно числу столбцов первой матрицы. 
+При умножении матриц получается матрица, число строк которой равно числу строк первой матрицы, 
+а число столбцов равно числу столбцов второй матрицы.*/
+
+int Prompt(string message) // запрос и ввод значений
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[,] FillRandomMatrix() // наполняем матрицу случайными значениями по заданным параметрам и выводим на экран
+{
+    int line = Prompt("Укажите количество строк матрицы >- ");
+    int column = Prompt("Укажите количество столбцов матрицы >- ");
+    int argMin = Prompt("Укажите диапазон значений матрицы: от >- ");
+    int argMax = Prompt("до (включительно) >- ");
+    int[,] matrix = new int[line, column];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(argMin, argMax);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+///
+int[,] MatrixMultiplication(int[,] A, int[,] B)
+{
+    int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+    if (A.GetLength(1) == B.GetLength(0))
+    {
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < B.GetLength(1); j++)
+            {
+                for (int k = 0; k < B.GetLength(0); k++)
+                {
+                    C[i, j] += A[i, k] * B[k, j];
+                }
+            }
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Матрицы не согласованы. Операция умножения матриц невыполнима");
+        System.Environment.Exit(001);
+    }
+        
+    return C;
+}
+
+    int[,] matrix1 = FillRandomMatrix();
+    int[,] matrix2 = FillRandomMatrix();
+    PrintMatrix(matrix1);
+    System.Console.WriteLine("");
+    PrintMatrix(matrix2);
+    System.Console.WriteLine("");
+    int[,] matrixM = MatrixMultiplication(matrix1, matrix2);
+    PrintMatrix(matrixM);
+
+
